aaf09572a885b1a97ca42a5ae57050c8d9b0ab9d
hursts <hursts@UK621350LT2.europe.FS.FUJITSU.com>
2017-07-26 14:53:01 +0100

    First commit of basic project from eclipse.

---
+++ src/main/java/com/brindleys/rest/App.java
@@ -0,0 +1,13 @@
+package com.brindleys.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
